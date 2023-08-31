57404a276ef8dffcf8678a1a4954c1d8f92cdcf8
kaishi <zerokara.imasara@gmail.com>
2023-08-31 09:24:34 +0900

    カレンダー設置 google font 設定

--- lib/ui/theme.dart
+++ lib/ui/theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyColor {
   static const blue = Color(0xFF0963ad);
@@ -8,17 +9,25 @@ class MyColor {
   static const dark = Color(0xFF4F4E4E);
   static const grey = Color(0xFFBCB6B6);
   static const lightGrey = Color(0xFFD9D9D9);
+  static const white = Color(0xFFFFFFFF);
 }
 
 class Themes {
   static final light = ThemeData(
+    fontFamily: 'Noto_Sans_JP',
     colorScheme: const ColorScheme.light(
       background: MyColor.lightGrey,
     ),
   );
   static final dark = ThemeData(
+    fontFamily: 'Noto_Sans_JP',
     colorScheme: const ColorScheme.dark(
       background: MyColor.dark,
     ),
   );
+}
+
+class MyTextStyle {
+  static const heading = TextStyle(fontSize: 32, fontWeight: FontWeight.w900);
+  static const medium = TextStyle(fontSize: 24, fontWeight: FontWeight.w400);
 }
--- lib/ui/widget/home_screen.dart
+++ lib/ui/widget/home_screen.dart
@@ -1,10 +1,19 @@
+import 'package:date_picker_timeline/date_picker_widget.dart';
 import 'package:edo_task/service/theme_service.dart';
+import 'package:edo_task/ui/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime _selectedDate = DateTime.now();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,41 +24,64 @@ class HomeScreen extends StatelessWidget {
             const SizedBox(
               height: 20,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                const Text(
-                  'Tasks',
-                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-                ),
-                const Spacer(),
-                // * テーマ切り替え
-                IconButton(
-                    onPressed: () {
-                      ThemeService().switchTheme();
-                    },
-                    icon: const FaIcon(
-                      FontAwesomeIcons.lightbulb,
-                      size: 28,
-                    )),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.notifications_none_outlined,
-                    size: 32,
-                  ),
-                ),
-                IconButton(
-                    onPressed: () {},
-                    icon: const FaIcon(
-                      FontAwesomeIcons.sliders,
-                      size: 32,
-                    ))
-              ],
-            )
+            _appBar(),
+            Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: DatePicker(
+                  DateTime.now(),
+                  locale: "ja_JP",
+                  height: 100,
+                  width: 70,
+                  selectionColor: MyColor.blue,
+                  initialSelectedDate: DateTime.now(),
+                  selectedTextColor: MyColor.white,
+                  dateTextStyle: MyTextStyle.medium,
+                  dayTextStyle: MyTextStyle.medium,
+                  monthTextStyle: MyTextStyle.medium,
+                  onDateChange: (date) {
+                    setState(() {
+                      _selectedDate = date;
+                    });
+                  },
+                )),
           ]),
         ),
       ),
+    );
+  }
+
+  Row _appBar() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        const Text(
+          'Tasks',
+          style: MyTextStyle.heading,
+        ),
+        const Spacer(),
+        // * テーマ切り替え
+        IconButton(
+            onPressed: () {
+              ThemeService().switchTheme();
+            },
+            icon: const FaIcon(
+              FontAwesomeIcons.lightbulb,
+              size: 28,
+            )),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.notifications_none_outlined,
+            size: 32,
+          ),
+        ),
+        IconButton(
+            onPressed: () {},
+            icon: const FaIcon(
+              FontAwesomeIcons.sliders,
+              size: 32,
+            ))
+      ],
     );
   }
 }
